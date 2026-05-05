8c7f83c2fcb63acca1ffdb82e4fa921be9454c21
amreenadams05 <amreenbasha05@gmail.com>
2026-05-05 17:30:41 +0000

    Update Init

--- social_media_functions/parse_metadata/__init__.py
+++ social_media_functions/parse_metadata/__init__.py
@@ -2,10 +2,12 @@ from .main_parser import (
     parse_metadata,
     tiktok_events,
     social_media_events,
+    StudentInputError,
 )
 
 __all__ = [
     "parse_metadata",
     "tiktok_events",
     "social_media_events",
+    "StudentInputError",
 ]
